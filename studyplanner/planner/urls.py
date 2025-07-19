eb91cf337fd1eaf27f2cbcc7b293f0475ec756a5
RoziKaji <rozi.kaji.esf@gmail.com>
2025-07-19 09:53:17 +0430

    edit_btn

--- studyplanner/planner/urls.py
+++ studyplanner/planner/urls.py
@@ -1,9 +1,12 @@
 from .views import *
 from django.urls import path
+from .views import subject_list, test, subject_edit
 
 app_name = "planner"
 
 urlpatterns = [
     path('index/', view=subject_list, name='subject_list'),
-    path('test/', test)
+    path('test/', test),
+    path('subjects/<int:subjecs_id>/edit/', subject_edit, name='subject_edit'),
+
 ]
--- studyplanner/planner/views.py
+++ studyplanner/planner/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from planner.models import subject
 from .forms import SubjectForm
+from django.urls import reverse
 
 def subject_list(request):
     subjects = subject.objects.all()
@@ -10,8 +11,16 @@ def add_subject(request):
     #if request.mode = 
     pass
 
-def index(request):
-    return render(request, "index.html")
+def subject_edit(request, subjecs_id):
+    item = get_object_or_404(subject, pk=subjecs_id)
+    if request.method == 'POST':
+        form = SubjectForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('planner:subject_list')
+    else:
+        form = SubjectForm(instance=item)
+    return render(request, 'edit_subject.html', {'form': form, 'subject': item})
 
 def test(request):
     return render(request, "test.html")
